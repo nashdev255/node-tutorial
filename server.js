e21286809ce1e1a226defda231bce96902bd00e5
nashdev255 <yutori1107@gmail.com>
2023-09-09 01:34:10 +0900

    good night

--- server.js
+++ server.js
@@ -10,13 +10,23 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(html);
 
-    if(req.method == 'GET') console.log("GET received");
-    if(req.method == 'POST') console.log("POST received");
-    if(req.method == 'DELETE') console.log("DELETE received");
+    if(req.method === 'GET') console.log("GET received");
+    if(req.method === 'POST') {
+        console.log("POST received");
+
+        var data = '';
+
+        req.on('data', (chunk) => {
+            data += chunk;
+        }).on('end', () => {
+            console.log(data);
+        });
+
+    }
 
     res.end();
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running at http://${ hostname }:${ PORT }/`);
+    console.log("Server has started running...");
 });
